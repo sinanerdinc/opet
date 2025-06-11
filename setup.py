a7186a958376979741c8b48b16d828411a871ce6
Sinan Erdinç <sinan.erdinc@btctrader.com>
2025-06-11 20:32:18 +0300

    improve doc and code quality

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="opet",
     author="Sinan Erdinc",
     author_email="hello@sinanerdinc.com",
-    version="0.1.0",
+    version="0.1.1",
     install_requires=["requests", "click"],
     description="A Python package that allows you to view fuel prices in Turkey based on cities.",
     long_description=long_description,
